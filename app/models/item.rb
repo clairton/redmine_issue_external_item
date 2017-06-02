acda8bc662bdefb272d5df7ea7dac83d1ee2b4dd
Clairton Rodrigo Heinzen <clairton.rodrigo@gmail.com>
2017-06-02 14:45:01 -0300

    Convert a string connection to a symbol

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
   #TODO load connection name by configuration
-  establish_connection "external_item_#{Rails.env}"
+  establish_connection "external_item_#{Rails.env}".to_sym
 
   attr_accessible :description, :key
 end
